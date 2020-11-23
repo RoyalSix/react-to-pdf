9f4d341cf263f4dd0b75df2c94cb8bf28241ae74
RoyalSix <jay.a.scottjr@biola.edu>
2020-11-22 18:18:12 -0800

    wip

--- functions/api.js
+++ functions/api.js
@@ -1,18 +1,5 @@
-import puppeteer from "puppeteer-core";
-import delay from 'buffer';
-const launchChrome = require("@serverless-chrome/lambda");
-
-const request = require("superagent");
-
-const getChrome = async () => {
-  const chrome = await launchChrome();
-  const response = await request.get(`${chrome.url}/json/version`).set("Content-Type", "application/json");
-  const endpoint = response.body.webSocketDebuggerUrl;
-  return {
-    endpoint,
-    instance: chrome
-  };
-};
+import delay from 'delay';
+const chromium = require('chrome-aws-lambda');
 
 exports.handler = async function (event, context) {
   const body = JSON.parse(event.body);
@@ -27,14 +14,14 @@ exports.handler = async function (event, context) {
     };
   }
 
-  const chrome = await getChrome();
-  console.log("Got Chrome");
-  const pageUrl = encodeURI(url);
-  console.log("Got page url as", pageUrl); // const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
-
-  const browser = await puppeteer.connect({
-    browserWSEndpoint: chrome.endpoint
+  const browser = await chromium.puppeteer.launch({
+    executablePath: await chromium.executablePath,
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    headless: chromium.headless,
   });
+  const pageUrl = encodeURI(url);
+  console.log("Got page url as", pageUrl);
   const page = await browser.newPage();
   await page.setDefaultNavigationTimeout(0);
   await page.goto(pageUrl, {
