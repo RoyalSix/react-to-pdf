0ccd7553a8113e1266a3c6b069cb3573eaac7a28
RoyalSix <jay.a.scottjr@biola.edu>
2020-11-22 17:42:08 -0800

    wip

--- functions/api.js
+++ functions/api.js
@@ -1,17 +1,13 @@
 import puppeteer from "puppeteer";
 import delay from 'buffer';
 const launchChrome = require("@serverless-chrome/lambda");
+
 const request = require("superagent");
 
 const getChrome = async () => {
   const chrome = await launchChrome();
-
-  const response = await request
-    .get(`${chrome.url}/json/version`)
-    .set("Content-Type", "application/json");
-
+  const response = await request.get(`${chrome.url}/json/version`).set("Content-Type", "application/json");
   const endpoint = response.body.webSocketDebuggerUrl;
-
   return {
     endpoint,
     instance: chrome
@@ -20,37 +16,47 @@ const getChrome = async () => {
 
 exports.handler = async function (event, context) {
   const body = JSON.parse(event.body);
-  const { url } = body;
+  const {
+    url
+  } = body;
+
   if (!url) {
     return {
       statusCode: 500,
       body: 'No URL specified'
-    }
+    };
   }
+
   const chrome = await getChrome();
   console.log("Got Chrome");
   const pageUrl = encodeURI(url);
-  console.log("Got page url as", pageUrl);
-  // const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
+  console.log("Got page url as", pageUrl); // const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
+
   const browser = await puppeteer.connect({
     browserWSEndpoint: chrome.endpoint
   });
   const page = await browser.newPage();
   await page.setDefaultNavigationTimeout(0);
-  await page.goto(pageUrl, { printBackground: true });
+  await page.goto(pageUrl, {
+    printBackground: true
+  });
   console.log("Navigated to page");
   await page.goto(pageUrl, {
     timeout: 20000,
     waitUntil: ['load', 'domcontentloaded', 'networkidle0', 'networkidle2']
-  })
+  });
   await delay(1000);
-  const pdf = await page.pdf({ format: "A4" });
+  const pdf = await page.pdf({
+    format: "A4"
+  });
   console.log("Generated pdf", pdf.length);
   await browser.close();
   return {
     statusCode: 200,
-    headers: { 'Content-type': 'application/pdf' },
+    headers: {
+      'Content-type': 'application/pdf'
+    },
     body: pdf.toString('base64'),
-    isBase64Encoded: true,
+    isBase64Encoded: true
   };
-}
+};
