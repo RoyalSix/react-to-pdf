8c7a26b7bb6e0aa7f1b9029987bbe56fc0c8c622
RoyalSix <jay.a.scottjr@biola.edu>
2020-11-22 16:59:22 -0800

    added pdf generation

--- functions/api.js
+++ functions/api.js
@@ -1,6 +1,34 @@
+import puppeteer from "puppeteer";
+import delay from 'buffer';
+
 exports.handler = async function (event, context) {
+  const body = JSON.parse(event.body)
+  const { url } = body;
+  if (!url) {
+    return {
+      statusCode: 500,
+      body: 'No URL specified'
+    }
+  }
+  const pageUrl = encodeURI(url);
+  console.log("Got page url as", pageUrl);
+  const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
+  const page = await browser.newPage();
+  await page.setDefaultNavigationTimeout(0);
+  await page.goto(pageUrl, { printBackground: true });
+  console.log("Navigated to page");
+  await page.goto(pageUrl, {
+    timeout: 20000,
+    waitUntil: ['load', 'domcontentloaded', 'networkidle0', 'networkidle2']
+  })
+  await delay(1000);
+  const pdf = await page.pdf({ format: "A4" });
+  console.log("Generated pdf", pdf.length);
+  await browser.close();
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" })
+    headers: { 'Content-type': 'application/pdf' },
+    body: pdf.toString('base64'),
+    isBase64Encoded: true,
   };
 }
